ce84503cbaf0624b517a19ae4576781440d70599
iachieve <mhalim80@hotmail.com>
2020-04-17 19:13:53 -0700

    Updates

---
+++ precache-manifest.8a91b6b74790b641f16b60eebab883fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87c168fe91baa4c8c1bd8d7b5a6955b",
+    "url": "/moviepedia/index.html"
+  },
+  {
+    "revision": "507631aa5e546ff91e2c",
+    "url": "/moviepedia/static/css/main.bca6b6bd.chunk.css"
+  },
+  {
+    "revision": "8d3b01b3b77c3e39a013",
+    "url": "/moviepedia/static/js/2.05bf6ec3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/moviepedia/static/js/2.05bf6ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507631aa5e546ff91e2c",
+    "url": "/moviepedia/static/js/main.b88bd938.chunk.js"
+  },
+  {
+    "revision": "1a07203fb8e35948882c",
+    "url": "/moviepedia/static/js/runtime-main.1f09d37f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.347074e6cbd4fc57cd707ece804c2529.js"
+  "/moviepedia/precache-manifest.8a91b6b74790b641f16b60eebab883fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moviepedia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
